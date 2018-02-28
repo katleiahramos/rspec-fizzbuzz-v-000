ef27ce7f2183c1ae51915bb5cd5a520a4b21439f
Katleiah <katleiahramos@gmail.com>
2018-02-28 21:17:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,8 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
   "Buzz"
-  #else
-  end
-  #if int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
+
   #  print "FizzBuzz"
 #  end
 end
