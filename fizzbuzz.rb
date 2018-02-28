3898dce424465a47d68b56a7c7bf294e0d900fc5
Katleiah <katleiahramos@gmail.com>
2018-02-28 21:02:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,3 +11,7 @@ def fizzbuzz(int)
  else
 
 end
+
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz()  # => You should get an ArgumentError
