40d3f7c9c644ec362640648f12d5b4c90f7f3828
Katleiah <katleiahramos@gmail.com>
2018-02-28 20:59:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,13 @@
-def fizzbuzz
-
-
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+    puts "FizzBuzz"
+  end
+  if int % 3 == 0 
+    puts "Fizz"
+  end 
+  if int % 5 == 0 
+    puts "Buzz"
+  end 
+ else 
+   
 end 
